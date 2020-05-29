72a8d8ae5e55035f5097549ab9dfb840ec245f36
Timothy Hull <timothyhull@gmail.com>
2020-05-29 16:28:27 -0700

    Refactor suggestions

--- restapi_test.py
+++ restapi_test.py
@@ -1,64 +1,50 @@
 #! /usr/bin/env python
 
+import os # attempt to read API key from the environment variable (OW_API_KEY)
+import sys # access an optional argument to write raw JSON output to a file
 import requests
 import json
 from getpass import getpass
 
 # insert your api key here for the location
-print('Enter your the City of where you want know the current weather.')
-city_name = input()
+print()
+city_name = input('Enter your the City of where you want know the current weather: ')
 
 
 # insert your api key here for the location
-print('Enter your the State whole name of where you want know the current weather.')
-state = input() 
+print()
+state = input('Enter your the State whole name of where you want know the current weather: ')
 
 # who do you want to send this information to?
 # print('To whom would you like to send this weather information to? Enter their email now.')
 # whoto = input()
 
-weather_api_key = getpass('enter you weather api key now.')
+weather_api_key = os.getenv('OW_API_KEY') # check for the existance of the environment variable (OW_API_KEY)
+if not weather_api_key: # if the environment variable (OW_API_KEY) returns None, prompt for an API key
+    weather_api_key = getpass('Enter your weather AKI key: ')
 
 # webexapikey = getpass('enter your webex api key now.')
 
 wurl = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state}&units=imperial&appid={weather_api_key}'
 
-payload = {}
-headers= {}
+headers= {
+    'Accept': 'application/json' # the API doc says it supports JSON, XML, and HTML so it's good to specify the one you want
+}
 
-response = requests.request("GET", wurl, headers=headers, data = payload)
+response = requests.request("GET", wurl, headers=headers)
 
-print(response.text.encode('utf8'))
+# Check for the presence of the argument 'debug' and, if present, write the raw data to a file
+# Usage: python rest_test.py debug
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'debug':
+        import json
+        with open(f'{sys.argv[0]}_response.json', 'w') as file:
+            file.write(json.dumps(response.json(), indent=2))
 
 message = response.json()
-# message = json.dumps(message_dict, indent= 4)
-# print(message)
+print()
 print(message['name'])
 print(message['main']['temp'])
 for weather in message['weather']:
-    for key, value in weather.items():
-        print(key['main'])
-        # if key == 'main':
-            # print(value)
-# 
-# print(message_dict['weather'])
-
-
-
-
-# url = "https://api.ciscospark.com/v1/messages"
-
-# payload = {
-     # "text": message,
-   # "toPersonEmail": whoto
-# }
-
-# headers = {
-    # 'Authorization': 'Bearer ' + webexapikey,
-    # 'Content-Type': 'application/json'
-# }
-
-# response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-
-# print(response.text.encode('utf8'))
-
+    print(weather['main'])
+print()
